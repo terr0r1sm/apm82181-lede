1fdbcba2d51c4f503dcc572303d58180ec04bf05
Christian Lamparter <chunkeey@googlemail.com>
2017-07-18 17:48:44 +0200

    build: sysupgrade-tar handle dtname
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

--- scripts/sysupgrade-tar.sh
+++ scripts/sysupgrade-tar.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+dtname=""
 board=""
 kernel=""
 rootfs=""
 outfile=""
+images=""
 err=""
 
 while [ "$1" ]; do
 	case "$1" in
 	"--board")
 		board="$2"
+		shift
+		shift
+		continue
+		;;
+	"--dtname")
+		dtname="$2"
 		shift
 		shift
 		continue
@@ -36,8 +44,8 @@ while [ "$1" ]; do
 	esac
 done
 
-if [ ! -n "$board" -o ! -r "$kernel" -a  ! -r "$rootfs" -o ! "$outfile" ]; then
-	echo "syntax: $0 [--board boardname] [--kernel kernelimage] [--rootfs rootfs] out"
+if [ ! -n "$board" -a ! -n "$dtname" ] && [ ! -r "$kernel" -a  ! -r "$rootfs" -o ! "$outfile" ]; then
+	echo "syntax: $0 [--board boardname] [--dtname dtname] [--kernel kernelimage] [--rootfs rootfs] out"
 	exit 1
 fi
 
@@ -51,17 +59,33 @@ if [ -z "$tmpdir" ]; then
 	exit 1
 fi
 
-mkdir -p "${tmpdir}/sysupgrade-${board}"
-echo "BOARD=${board}" > "${tmpdir}/sysupgrade-${board}/CONTROL"
-[ -z "${rootfs}" ] || cp "${rootfs}" "${tmpdir}/sysupgrade-${board}/root"
-[ -z "${kernel}" ] || cp "${kernel}" "${tmpdir}/sysupgrade-${board}/kernel"
+[ -z "${board}" ] || {
+	mkdir -p "${tmpdir}/sysupgrade-${board}"
+	echo "BOARD=${board}" > "${tmpdir}/sysupgrade-${board}/CONTROL"
+	[ -z "${rootfs}" ] || cp "${rootfs}" "${tmpdir}/sysupgrade-${board}/root"
+	[ -z "${kernel}" ] || cp "${kernel}" "${tmpdir}/sysupgrade-${board}/kernel"
+	images+="sysupgrade-${board}"
+}
+
+[ -z "${dtname}" ] || {
+	mkdir -p "${tmpdir}/sysupgrade-${dtname}"
+	echo "BOARD=${dtname}" > "${tmpdir}/sysupgrade-${dtname}/CONTROL"
+	if [ -z "${board}" ]; then
+		[ -z "${rootfs}" ] || cp "${rootfs}" "${tmpdir}/sysupgrade-${dtname}/root"
+		[ -z "${kernel}" ] || cp "${kernel}" "${tmpdir}/sysupgrade-${dtname}/kernel"
+	else
+		[ -z "${rootfs}" ] || ln -s "../sysupgrade-${board}/root" "${tmpdir}/sysupgrade-${dtname}/root"
+		[ -z "${kernel}" ] || ln -s "../sysupgrade-${board}/kernel" "${tmpdir}/sysupgrade-${dtname}/kernel"
+	fi
+	images+=" sysupgrade-${dtname}"
+}
 
 mtime=""
 if [ -n "$SOURCE_DATE_EPOCH" ]; then
 	mtime="--mtime=@${SOURCE_DATE_EPOCH}"
 fi
 
-(cd "$tmpdir"; tar cvf sysupgrade.tar sysupgrade-${board} ${mtime})
+(cd "$tmpdir"; tar cvf sysupgrade.tar ${images} ${mtime})
 err="$?"
 if [ -e "$tmpdir/sysupgrade.tar" ]; then
 	cp "$tmpdir/sysupgrade.tar" "$outfile"
